d8b20edbebd379589286916c12cd9666a6df4696
EihabObeidat <86022055+EihabObeidat@users.noreply.github.com>
2022-01-18 23:51:25 +0200

    test2

--- ErpSystem.core/Class1.cs
+++ ErpSystem.core/Class1.cs
@@ -4,11 +4,8 @@ namespace ErpSystem.core
 {
     public class Class1
     {
-<<<<<<< HEAD
-        public string eihab = "Eihab";
-=======
-                public int MyProperty { get; set; }
+        public string eihab {get; set;}
+        public int MyProperty { get; set; }
 
->>>>>>> 1625b1acf921723046a44a5a3c68d6b0427d194e
     }
 }
